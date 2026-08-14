fb835c5adb84d1bf27a77d120a9d67708b990b35
Alhagie Sillah <alhagiesillah015@gmail.com>
2026-08-14 10:21:51 +0000

    Added Landing page and initial

---
+++ firebaseconfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBDX7gQBE_ChTzBxs3hPTGKhrMrbzgy_DU",
+  authDomain: "quiknotes-c6efb.firebaseapp.com",
+  projectId: "quiknotes-c6efb",
+  storageBucket: "quiknotes-c6efb.firebasestorage.app",
+  messagingSenderId: "299188047990",
+  appId: "1:299188047990:web:621b65b87c36876bf5cf57"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/App.jsx
+++ src/App.jsx
@@ -1,93 +1,23 @@
 import React from 'react'
-import {BrowserRouter,Routes,Route,Link, Navigate} from 'react-router-dom'
-import Signup from './Pages/Signup'
-import  Signin from './Pages/Signin'
-import Home from './Pages/Home'
-import "./styles/global.css";
-import { useAuth } from './context/AuthContext'
-import Dashbord from './Pages/Dashbord'
-
-
-
-
-function ProtectedRoute({children}){
-  const{user,loading}= useAuth();
-  if(loading){
-  return <p style={{padding:"24"}}>Loading ...</p>
-}
-  return user ? children : <Navigate to="signin" 
-  replace/>
-}
-
-
-  export default function App() {
-  
-
-const {user,loading}= useAuth();
-
-if(loading){
-  return <p style={{padding:"24"}}>Loading ...</p>
-}
-  
-   
+import Landing from './routes/Landing'
+import { Routes,Route,Navigate, } from 'react-router-dom'
+import Login from './routes/Login'
+import Signup from './routes/Signup'
+function App() {
   return (
-    <>
-    <BrowserRouter>
     <div>
-      <h1>Firebase Auth Demo</h1>
-      <nav style={{marginBottom: "24px"}}>
-        {!user && (
-          <>
-           <Link className='btn' to='/signup'>
-        Signup
-        </Link>
-        <Link className='btn' to='/signin'>
-        Signin
-        </Link>
-       
-        </>
-        )}
-        { user &&
-        <>
-        <Link className='btn' to='/
-        dashbor'>
-         Dashbord
-        </Link>
-        </>
-        }
-      </nav>
+      <Routes>
+        <Route path='/' element={<Landing/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+
+      </Routes>
     </div>
-    <Routes>
-       <Route path='/home' element={<Home/>}/>
-
-      <Route path='/signup'element={user ? <Navigate to="/dashbord"/>:<Signup/>}/>
-     
-     
-     
-     
- 
-      <Route path='/signin'element={user ? <Navigate to="/dashbord"/>:<Signin/>} />
-     
-      
-     
-      
-
-      
-
-       
-
-
-     <Route path='/dashbord'
-      element={<ProtectedRoute>
-        <Dashbord/>
-      </ProtectedRoute>}/>
-      
-    </Routes>
-    </BrowserRouter>
-    </>
-      
-  
   )
 }
 
+export default App
 
+      
+
+       
